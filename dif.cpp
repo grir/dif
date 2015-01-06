5521cef018c570b6149b571af002b6f7e633cf47
i <irus.grinis@gmail.com>
2015-01-06 23:41:13 +0200

    modified:   dif.cpp

--- dif.cpp
+++ dif.cpp
@@ -19,8 +19,9 @@ std::piecewise_constant_distribution<double>
      d (intervals.begin(),intervals.end(),weights.begin());
 
 std::normal_distribution<double>   nd (0,1);
-std::bernoulli_distribution bd(0.05);
+std::bernoulli_distribution bd(0.005);
 
+std::uniform_real_distribution<> ud(-1, 1);
 
 void generateOuter(int n, vector<double>& outer){
 
@@ -49,7 +50,9 @@ void timeStep(double dt, double vOuter, double vInner, vector<double>& outer, ve
     // update outers:
     for(int i = 0;i < outer.size(); i++){
        double old = outer[i];
-       outer[i] += nd(g1) * sqrt(dt) * vOuter;
+//       outer[i] += nd(g1) * sqrt(dt) * vOuter;
+       outer[i] += ud(g1)  * vOuter;
+      
        if (outer[i] < -2.0)
           outer[i] = 4 + outer[i];
        else if (outer[i] > 2.0)
@@ -73,8 +76,8 @@ void timeStep(double dt, double vOuter, double vInner, vector<double>& outer, ve
        }
        else {
          double old = inner[i];
-         inner[i] += nd(g1) * sqrt(dt) * vInner;
-
+         //inner[i] += nd(g1) * sqrt(dt) * vInner;
+         inner[i] += ud(g1) *  vInner;
          if ( (inner[i] > 1.0) || (inner[i] < -1.0))
           inner[i] = old;
        }
@@ -92,27 +95,31 @@ int main(){
    double A=a-1, B=b+1;
    vector<double> outer;
    vector<double> inner;
-   double T=1;
+   double T=10;
    double dt = T/1000;
    double vInner=0.1;
    double vOuter=0.1;
    
 
-   int N = 10000;
+   int N = 100;
    generateOuter(N, outer);
-   
+
+ //  for(int i=0;i<outer.size();i++)
+ //    cout << outer[i] << endl;   
+
    int maxI = 0;
    int minI = N;
+   double avg=0;
    for(double t=0; t<=T;t+=dt)   {
    
       timeStep(dt,  vOuter,  vInner,  outer,  inner);
       if(maxI < inner.size()) maxI = inner.size();
       if(minI > inner.size()) minI = inner.size();
-      
+      avg+=inner.size();
    
    }
 
-   cout << minI << " <-> " << maxI << endl;
+   cout << minI << " <-> " << maxI << " : " << avg /1000 << endl;
     
   return 0;
 }
